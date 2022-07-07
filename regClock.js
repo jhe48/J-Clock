1322c246bc66bd5a288b8c63e796a2cdb03d3fd7
Jacky He <78707612+jhe48@users.noreply.github.com>
2022-07-06 23:36:40 -0400

    Replaced setInterval function
    I used a recursive RequestAnimationFrame method instead of setInterval function.

--- regClock.js
+++ regClock.js
@@ -6,16 +6,20 @@ const hr = document.querySelector('#hr');
 const min = document.querySelector('#mn');
 const sec = document.querySelector('#sc');
 
-// Set Interval to move arms.
-setInterval(() => {
-    let day = new Date();
-    let ss = day.getSeconds() + (day.getMilliseconds() / 1000);
-    let mm = day.getMinutes() + (ss / 60);
-    let hh = day.getHours() + (mm / 60);
+// Move Arms
+function moveArms() {
+    requestAnimationFrame(moveArms);
+    const day = new Date();
+    const ss = day.getSeconds() + (day.getMilliseconds() / 1000);
+    const mm = day.getMinutes() + (ss / 60);
+    const hh = day.getHours() + (mm / 60);
 
     hr.style.transform = `rotateZ(${hh * 30}deg)`;
     mn.style.transform = `rotateZ(${mm * deg}deg)`;
     sc.style.transform = `rotateZ(${ss * deg}deg)`;
-});
+}
+// Don't Use Set Interval
+moveArms();
+
 
 
